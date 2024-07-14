d236dff9916ceeff20b2eb1d3e0d0cebc56d5056
Akbar Iskanderov <iskanderovofficial@gmail.com>
2024-07-14 23:22:29 +0500

    added cart badge and carts save to localstorage

--- src/redux/reducer/reducer.js
+++ src/redux/reducer/reducer.js
@@ -1,21 +1,45 @@
-export const initialState = {
+const loadState = () => {
+  try {
+    const serializedState = localStorage.getItem('cartState');
+    if (serializedState === null) {
+      return undefined;
+    }
+    return JSON.parse(serializedState);
+  } catch (err) {
+    return undefined;
+  }
+};
+
+
+const saveState = (state) => {
+  try {
+    const serializedState = JSON.stringify(state);
+    localStorage.setItem('cartState', serializedState);
+  } catch (err) {
+  }
+};
+
+const initialState = loadState() || {
   cart: {
     items: [],
   },
 };
 
 export const reducer = (state = initialState, action) => {
+  let newState;
   switch (action.type) {
     case "ADD_TO_CART":
-      return {
+      newState = {
         ...state,
         cart: {
           ...state.cart,
           items: [...state.cart.items, action.payload],
         },
       };
+      saveState(newState);
+      return newState;
     case "DELETE_FROM_CART":
-      return {
+      newState = {
         ...state,
         cart: {
           ...state.cart,
@@ -24,6 +48,8 @@ export const reducer = (state = initialState, action) => {
           ),
         },
       };
+      saveState(newState);
+      return newState;
     default:
       return state;
   }
